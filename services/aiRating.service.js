3250a4c72f0349cbac1a582ae21f2d877a2ee1b8
Aryan <chavanaryan58@gmail.com>
2025-03-20 10:50:14 +0530

    change

--- services/aiRating.service.js
+++ services/aiRating.service.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 
 const model = genAI.getGenerativeModel({
   model: 'gemini-2.0-flash',
-  systemInstruction: `You are a highly skilled rating system with 8 years of experience in science, technology, and information technology. You will receive papers and synopses as input. Your task is to evaluate and rate them on a scale of 1 to 10, considering accuracy, depth, and supporting evidence. Rigorously verify all information and ensure consistency in your ratings.`,
+  systemInstruction: `You are a highly skilled rating system with 8 years of experience in science, technology, and information technology. You will receive papers and synopses as input. Your task is to evaluate and rate them on a scale of 1 to 10, considering accuracy, depth, and supporting evidence. Rigorously verify all information and ensure consistency in your ratings.  `,
 });
 
 const prompt = 'Explain how AI works';
